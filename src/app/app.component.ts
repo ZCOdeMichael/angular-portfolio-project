927cad7ce26aa609bbf00147fd81a48f4941d2b9
ZCodeMichael <zcodemichael@gmail.com>
2021-08-08 04:21:15 +0000

    added basic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  
+  constructor(private elemRef: ElementRef){
+    
+  }
+
   title = 'angular-portfolio-project';
+  
+  navbarFixed: boolean = false;
+  atTopPage: boolean = true;
+  currPos = window.pageYOffset;
+
+  @HostListener('window:scroll', ['$event']) onScroll() {
+    
+    if(document.documentElement.scrollTop > this.currPos){
+      this.navbarFixed = false;
+    }else{
+      this.navbarFixed = true;
+    }
+    this.currPos = document.documentElement.scrollTop;
+    
+    let pos = (document.documentElement.scrollTop || document.body.scrollTop) + document.documentElement.offsetHeight;
+    let max = document.documentElement.scrollHeight;
+    if(pos == max){
+      this.atTopPage = true;  
+    }else{
+      this.atTopPage = false;
+    }
+    console.log(this.atTopPage);
+    
+  }
 }
